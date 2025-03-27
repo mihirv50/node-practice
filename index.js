3cab4631a6cb21fe16919acb07fe72e7e964d75a
Mihir Vyas <mihirv50@gmail.com>
2025-03-27 12:02:44 +0530

    Added middleware to check number of requests

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
 const express = require("express");
 const app = express();
 
+let requestCount = 0;
 
-app.get("/sum/:a/:b",(req,res)=>{
-    let {a,b} = req.params;
-    a = Number(a);
-    b = Number(b);
-    let ans = a+b;
-    res.send({
-        result:ans
-    });
+app.use((req, res, next) => {
+  requestCount += 1;
+    next();
+});
+app.get("/count",(req,res)=>{
+    res.json({
+        count:requestCount
+    })
 })
-app.get("/substract/:a/:b",(req,res)=>{
-    let {a,b} = req.params;
-    a = Number(a);
-    b = Number(b);
-    let ans = a-b;
-    res.send({
-        result: ans
+app.get("/sum/:a/:b", (req, res) => {
+  let { a, b } = req.params;
+  a = Number(a);
+  b = Number(b);
+  let ans = a + b;
+  res.send({
+    result: ans,
+  });
+});
+app.get("/substract/:a/:b", (req, res) => {
+  let { a, b } = req.params;
+  a = Number(a);
+  b = Number(b);
+  let ans = a - b;
+  res.send({
+    result: ans,
+  });
+});
+app.get("/multiply/:a/:b", (req, res) => {
+  let { a, b } = req.params;
+  a = Number(a);
+  b = Number(b);
+  let ans = a * b;
+  res.send({
+    result: ans,
+  });
+});
+app.get("/divide/:a/:b", (req, res) => {
+  let { a, b } = req.params;
+  a = Number(a);
+  b = Number(b);
+  if (b === 0) {
+    return res.status(400).json({
+      error: "Cannot divide by 0",
     });
-})
-app.get("/multiply/:a/:b",(req,res)=>{
-    let {a,b} = req.params;
-    a = Number(a);
-    b = Number(b);
-    let ans = a*b;
-    res.send({
-        result: ans
-    });
-})
-app.get("/divide/:a/:b",(req,res)=>{
-    let {a,b} = req.params;
-    a = Number(a);
-    b = Number(b);
-    if(b===0){
-        return res.status(400).json({
-            error:"Cannot divide by 0"
-        })
-    }
-    let ans = Math.floor(a/b);
-    res.send({
-        result: ans
-    });
-})
-app.listen(3000)
+  }
+  let ans = Math.floor(a / b);
+  res.send({
+    result: ans,
+  });
+});
+app.listen(3000);
