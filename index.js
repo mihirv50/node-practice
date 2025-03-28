237e43938d088ababe7271c7b99accdf10c73bd7
Mihir Vyas <mihirv50@gmail.com>
2025-03-28 12:42:43 +0530

    replaced token with jwts

--- index.js
+++ index.js
@@ -1,80 +1,82 @@
 const express = require("express");
 const app = express();
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "randommihir"
 
 let users = [];
 
 // Random Token
-const generateToken = () => {
-  const options = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
-  let token = "";
-  for (let i = 0; i < 32; i++) {
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-};
+// const generateToken = () => {
+//   const options = [
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z",
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//     "0",
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//   ];
+//   let token = "";
+//   for (let i = 0; i < 32; i++) {
+//     token += options[Math.floor(Math.random() * options.length)];
+//   }
+//   return token;
+// }; --- using jwt now
 
 app.use(express.json());
 
@@ -104,8 +106,11 @@ app.post("/signin", (req, res) => {
     (u) => u.username === username && u.password === password
   );
   if (foundUser) {
-    const token = generateToken();
-    foundUser.token = token;
+    // const token = generateToken();
+    const token = jwt.sign({
+      username:username
+    },JWT_SECRET)
+    // foundUser.token = token; --> using jwt now
     res.json({
       msg: token,
     });
@@ -121,7 +126,9 @@ app.post("/signin", (req, res) => {
 
 app.get("/me",(req,res)=>{
   const token = req.headers.token;
-  let foundUser = users.find(u=>u.token===token);
+  const decodedinformation = jwt.verify(token,JWT_SECRET);
+  const username = decodedinformation.username;
+  let foundUser = users.find(u=>u.username===username);
   if(foundUser){
     res.json({
       username:foundUser.username,
